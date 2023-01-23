79c08625c5525e55a4492eae4da4a7c4694d76c6
Juan Pablo <jppd0657@gmail.com>
2023-01-22 19:51:24 -0700

    creds secured

--- query.js
+++ query.js
@@ -1,13 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-analytics.js";
+import { apiKey } from './creds.js';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
+  apiKey: apiKey,
   authDomain: "aarb-testing.firebaseapp.com",
   projectId: "aarb-testing",
   storageBucket: "aarb-testing.appspot.com",
@@ -17,5 +18,30 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+const db = getFirestore(app);
+
+// Get data
+
+var sections = [];
+window.sections = sections;
+
+async function queryData() {
+    const querySnapshot = await getDocs(collection(db, "mainPage"));
+
+    querySnapshot.forEach((doc) => {
+        sections.push(doc.data());
+    });
+
+    hero__slogan.innerHTML = sections[2]['org_slogan'];
+}
+
+queryData();
+console.log(sections);
+
+// header queries
+
+const hero__slogan = document.querySelector('#hero__slogan');
