684ffac39cdac89ba69037e4cd0bd7b231709752
Juan Pablo <jppd0657@gmail.com>
2023-01-22 20:02:15 -0700

    nothing new

--- query.js
+++ query.js
@@ -1,12 +1,49 @@
-// Import the functions you need from the SDKs you need
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-analytics.js";
+// import { apiKey } from './creds.js';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: apiKey,
+//   authDomain: "aarb-testing.firebaseapp.com",
+//   projectId: "aarb-testing",
+//   storageBucket: "aarb-testing.appspot.com",
+//   messagingSenderId: "78881099467",
+//   appId: "1:78881099467:web:5b0d9bed430a9b0519554e",
+//   measurementId: "G-JG1VN5RNSQ"
+// };
+
+// // Initialize Firebase
+
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// const db = getFirestore(app);
+
+// // Get data
+
+// var sections = [];
+
+// async function queryData() {
+//     const querySnapshot = await getDocs(collection(db, "mainPage"));
+
+//     querySnapshot.forEach((doc) => {
+//         sections.push(doc.data());
+//     });
+
+//     return sections;
+// }
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-analytics.js";
+// import { firestore } from 'https://www.gstatic.com/firebasejs/9.16.0/firebase-firestore.js';
+import { firestore }
 import { apiKey } from './creds.js';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: apiKey,
   authDomain: "aarb-testing.firebaseapp.com",
@@ -17,31 +54,18 @@ const firebaseConfig = {
   measurementId: "G-JG1VN5RNSQ"
 };
 
-// Initialize Firebase
-
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-const db = getFirestore(app);
-
-// Get data
+const db = firestore(app);
 
 var sections = [];
-window.sections = sections;
 
 async function queryData() {
-    const querySnapshot = await getDocs(collection(db, "mainPage"));
+    const querySnapshot = await db.collection("mainPage").get();
 
-    querySnapshot.forEach((doc) => {
+    querySnapshot.docs.forEach((doc) => {
         sections.push(doc.data());
     });
-
-    hero__slogan.innerHTML = sections[2]['org_slogan'];
+    return sections;
 }
-
-queryData();
-console.log(sections);
-
-// header queries
-
-const hero__slogan = document.querySelector('#hero__slogan');
