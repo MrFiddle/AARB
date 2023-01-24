7a6f16e938b75192e498195f39b2ebe5996a3537
Juan Pablo <jppd0657@gmail.com>
2023-01-24 13:47:02 -0700

    main page queries finished

--- query.js
+++ query.js
@@ -1,46 +1,3 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
-// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-analytics.js";
-// import { apiKey } from './creds.js';
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: apiKey,
-//   authDomain: "aarb-testing.firebaseapp.com",
-//   projectId: "aarb-testing",
-//   storageBucket: "aarb-testing.appspot.com",
-//   messagingSenderId: "78881099467",
-//   appId: "1:78881099467:web:5b0d9bed430a9b0519554e",
-//   measurementId: "G-JG1VN5RNSQ"
-// };
-
-// // Initialize Firebase
-
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
-// const db = getFirestore(app);
-
-// // Get data
-
-// var sections = [];
-
-// async function queryData() {
-//     const querySnapshot = await getDocs(collection(db, "mainPage"));
-
-//     querySnapshot.forEach((doc) => {
-//         sections.push(doc.data());
-//     });
-
-//     return sections;
-// }
-
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
-// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-analytics.js";
-// // import { firestore } from 'https://www.gstatic.com/firebasejs/9.16.0/firebase-firestore.js';
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-analytics.js";
 import { getFirestore, doc, getDoc, getDocs, onSnapshot, collection } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-firestore.js";
@@ -62,15 +19,7 @@ const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 var sections = [];
-
-async function queryData() {
-    const querySnapshot = await getDocs(collection(db, "mainPage"));
-
-    querySnapshot.docs.forEach((doc) => {
-        sections.push(doc.data());
-    });
-    return sections;
-}
+var footer = [];
 
 // body
 
@@ -112,16 +61,51 @@ const footerContact__phone = document.querySelector('#footer-contact__phone');
 const footerContact__mail = document.querySelector('#footer-contact__mail');
 const footer__year = document.querySelector('#footer__year');
 
+async function queryData() {
+    const querySnapshot = await getDocs(collection(db, "mainPage"));
+
+    querySnapshot.docs.forEach((doc) => {
+        sections.push(doc.data());
+    });
+    return sections;
+}
+
+async function queryFooter() {
+    const querySnapshot = await getDocs(collection(db, "footer"));
+    
+    querySnapshot.docs.forEach((doc) => {
+        footer.push(doc.data());
+    });
+    return footer;
+}
+
 queryData().then((sections) => {
   console.log(sections);
   // hero
   hero__slogan.innerHTML = sections[2]['org_slogan'];
+  // noticias
+  fNew__title.innerHTML = sections[1]['title'];
+  fNew__date.innerHTML = sections[1]['date'];
   // nosotros
   nosotros__content.innerHTML = sections[0]['content'];
-  body.style.display = 'block';
+    // nosotros__img.src = sections[0]['img'];
   // servicios
-
+  servicios__content.innerHTML = sections[3]['content'];
+    // servicios__img.src = sections[3]['img'];
   // contacto
-
+  contactoaddress__calle.innerHTML = sections[1]['address'];
+  contacto__horario.innerHTML = 'Horario: ' + sections[1]['horario'];
+  contacto__maps.href = sections[1]['maps'];
+  contacto__facebook.href = sections[1]['facebook'];
+  contacto__telefono.href = 'tel:' + sections[1]['phone'];
+  contacto__correo.href = 'mailto:' + sections[1]['email'];
+  contacto_wha.href = 'https://wa.me/' + sections[1]['whatsapp'];
   // footer
+  body.style.display = 'block';
+  queryFooter().then((footer) => {
+    footer__content.innerHTML = footer[0]['content'];
+    footerContact__phone.innerHTML = footer[0]['phone'];
+    footerContact__mail.innerHTML = footer[0]['email'];
+    footer__year.innerHTML = new Date().getFullYear();
+  });
 });
